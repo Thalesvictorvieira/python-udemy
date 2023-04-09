faacb94db86b6a2c218d2badc55c4cb3483b9798
Thales victor vieira <114888655+Thalesvictorvieira@users.noreply.github.com>
2023-04-09 19:44:19 -0300

    Create aul6.py

---
+++ aul6.py
@@ -0,0 +1,9 @@
+# Concatenaçao e juntar dois numeros literalmente exemplo 1 + 1 = 11 
+concatenacao = 'Thales' + 'victor'
+print(concatenacao)
+
+# * faz a repetição
+a_dez_vezes = 'A' * 10
+tres_vezes_Thales = 3 * 'Thales'
+print(a_dez_vezes)
+print(tres_vezes_Thales)
