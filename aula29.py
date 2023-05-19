f138439edd9d6bc5c7e281394c6a39906bc58234
Thales victor vieira <114888655+Thalesvictorvieira@users.noreply.github.com>
2023-05-19 15:31:42 -0300

    aula31

--- aula29.py
+++ aula29.py
@@ -4,5 +4,11 @@ i = 0
 while i < len(string):
     letra = string[i]
 
+    if letra == ' ':
+        break
+
     print(letra)
     i += 1 
+else:
+    print('Não encontri um espaço na string.')
+print('Fora do while.')
---
+++ aula31.py
@@ -0,0 +1,7 @@
+texto = 'python'
+i = 0 
+tamanho_string = len(texto)
+
+while i < tamanho_string:
+    print(texto[i], 1)
+    i += 1
