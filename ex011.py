d3822433e90cd8d562cc23ae128ff3cb201869d7
Thales victor vieira <114888655+Thalesvictorvieira@users.noreply.github.com>
2023-05-28 15:22:42 -0300

    Update ex011.py

--- ex011.py
+++ ex011.py
@@ -2,16 +2,31 @@ lista = []
 while True:
     valor = input('Digite a opçao que vc quer\n[i]inserir [a]apagar [l]listar : ')
     if valor == 'i':
-        ivalor = input('Você escolheu a opçao inserir \n Digite o valor que você quer inserir: ' )
+        ivalor = input('Você escolheu a opçao inserir'
+                       ' Digite o valor que você quer inserir: ' )
         lista.append(ivalor)
-        print(f'você adicionou {ivalor} na lista')
+        print(f'você adicionou ({ivalor}) na lista')
     elif valor == 'a':
         print('Você escolheu apagar ')
-        avalor = input('Digite o indicie que Você quer apagar ')
+        avalor = input('Digite o indicie que Você quer apagar:')
         print(lista)
-        
         try:
-            indicie = int(valor)
+            indicie = int(avalor)
             del lista[indicie] 
         except ValueError:
            print('Você so pode digitar numero inteiro ')
+        except IndexError:
+            print('esse indicie nao existe')
+        except Exception:
+            print('Erro desconhecido ')
+        
+    elif valor == 'l':
+        print('Você escolheu a opçao ler lista')
+        
+        if len(lista) == 0:
+         print('Não tem nada para eu listar.')
+        for i, valor in enumerate(lista):
+            print(i,valor)
+    else:
+        print('Digite a opçpes [i] ou [a] ou [l] '
+              '')
