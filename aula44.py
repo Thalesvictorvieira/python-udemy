b1ff411f473223d88e1e66b13156594d907a47e8
Thales victor vieira <114888655+Thalesvictorvieira@users.noreply.github.com>
2023-06-11 13:55:16 -0300

    aula45.py

---
+++ aula44.py
@@ -0,0 +1,9 @@
+# Descinoactanebti e chamadas
+# De nétedos e funcoes 
+string = 'ABCD'
+lista =['Maria', 'Helena', 'Eduarda', ]
+tupla = 'phyton', 'é', 'melhor que js'
+
+a, b, c = lista
+print(a,c)
+    print(*lista, sep='\n')
---
+++ aula45.py
@@ -0,0 +1,10 @@
+'''
+Operação ternária(conficional de uma linha )
+<Valor> if <codicao> else <outro Valor>
+'''
+#condicao = 10 == 10
+#Variavel = 'Valor' if False else 'Outro valor'
+#print(Variavel)
+digito = 9 
+novo = digito if digito<= 9 else 0
+print(novo)
